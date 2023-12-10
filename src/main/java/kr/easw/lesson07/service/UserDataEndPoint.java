ec90796f3dfc3b79bb4df2506143079deee255a1
Kim-byeonghun <74524187+Kim-byeonghun@users.noreply.github.com>
2023-12-10 23:25:22 +0900

    Update UserDataEndPoint.java

--- src/main/java/kr/easw/lesson07/service/UserDataEndPoint.java
+++ src/main/java/kr/easw/lesson07/service/UserDataEndPoint.java
@@ -1,1 +1,28 @@
+package kr.easw.lesson07.service;
 
+import kr.easw.lesson07.model.RemoveUserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserDataEndPoint {
+
+    private final UserDataService userDataService;
+
+    @GetMapping("/list")
+    public List<String> listUsers() {
+        return userDataService.userList();
+    }
+
+    @PostMapping("/remove")
+    public ResponseEntity<String> removeUser(@RequestBody RemoveUserDto removeUser) {
+        userDataService.deleteUser(removeUser.getUserId());
+        return ResponseEntity.ok().body(removeUser.getUserId());
+    }
+
+}
